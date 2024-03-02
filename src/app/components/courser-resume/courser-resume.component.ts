9deb5b566da0ea3a325812e0f27999527564c934
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-03-02 19:56:39 +0300

    update

---
+++ src/app/components/courser-resume/courser-resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courser-resume',
+  templateUrl: './courser-resume.component.html',
+  styleUrls: ['./courser-resume.component.css']
+})
+export class CourserResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
