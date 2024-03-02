80fbd7c5999f74b036bb8b0e8a27407dd95a864b
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-03-02 19:55:13 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourserInfoComponent } from './components/courser-info/courser-info.component';
+import { CourserResumeComponent } from './components/courser-resume/courser-resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", pathMatch:"full", redirectTo:"courserInfo"},
+  {path:"courserInfo", component:CourserInfoComponent},
+  {path:"courserResume", component:CourserResumeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
