9e8e911745fe9c8a78c0548ff23222bc61af7d1a
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-03-02 19:55:26 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplyCourseComponent } from './components/apply-course/apply-course.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CourserInfoComponent } from './components/courser-info/courser-info.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { CourserResumeComponent } from './components/courser-resume/courser-resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplyCourseComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    CourserInfoComponent,
+    PortfolioComponent,
+    CourserResumeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
